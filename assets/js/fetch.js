ec26ed03b8f163f8ca6b76c485fed7c627fb98d2
wesleylo <weslo404@gmail.com>
2022-06-01 20:12:45 -0400

    Add episode links

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -1,4 +1,5 @@
 var curr_film = 0;
+var appears_in = '';
 
 document.getElementById("btn-prev").addEventListener("click", prev);
 document.getElementById("btn-next").addEventListener("click", next);
@@ -18,8 +19,15 @@ function next() {
   fetchFilm(curr_film);
 }
 
+function search(id) {
+  curr_film = id
+  fetchFilm(curr_film);
+}
+
 function fetchFilm(film_id) {
   // console.log(film_id);
+
+
   fetch('https://swapi.dev/api/films')
   .then(data => {
     return data.json();
@@ -36,17 +44,34 @@ function fetchFilm(film_id) {
     document.querySelector('#release_date').innerHTML = films.results[film_id].release_date;
     document.querySelector('#opening_crawl').innerHTML = films.results[film_id].opening_crawl;
 
-    for(let i=0; i<4; i++){ // add first four characters
+    for(let i=0; i<4; i++) { // add first four characters
       fetch(films.results[film_id].characters[i])
       .then(data => {
         return data.json();
       })
       .then(person => {
         console.log(person);
-        // console.log('#name'.concat(i));
+        appears_in = '';
+        for(let i=0; i<3; i++) {
+           appears_in = appears_in + '<a href="#" onclick="clickLink(this)" class="episode_link" id="' + person.name + i + '">Episode '; // add onclick get link text
+           appears_in += person.films[i][person.films[i].length -2]; // get second to last char of film link
+
+           if(i!=2) appears_in = appears_in + '</a>, ';
+           console.log(person.films[i]);
+
+        }
+        console.log(appears_in);
+        document.querySelector('#character-films'.concat(i)).innerHTML = appears_in;
+
         document.querySelector('#name'.concat(i)).innerHTML = person.name;
       })
     }
 
   });
+}
+function clickLink(link) {
+  var t = link.innerText;
+  // alert(t[t.length - 1]);
+  // alert(parseInt(t[t.length - 1])-1); // get film index
+  search(parseInt(t[t.length - 1])-1);
 }
